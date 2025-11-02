817c3369fd226cea3a448ffd2f753dd00f1e1022
Evan Montgomery-Recht <montge@mianetworks.net>
2025-11-01 20:14:19 -0400

    🧹 Remove all CMake/build artifacts (1200+ files)

--- tests/cpp/build/test_inputparser[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/path/to/asterix/tests/cpp/build/test_inputparser[1]_tests.cmake")
-  include("/path/to/asterix/tests/cpp/build/test_inputparser[1]_tests.cmake")
-else()
-  add_test(test_inputparser_NOT_BUILT test_inputparser_NOT_BUILT)
-endif()
--- tests/cpp/build/test_utils[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/path/to/asterix/tests/cpp/build/test_utils[1]_tests.cmake")
-  include("/path/to/asterix/tests/cpp/build/test_utils[1]_tests.cmake")
-else()
-  add_test(test_utils_NOT_BUILT test_utils_NOT_BUILT)
-endif()
